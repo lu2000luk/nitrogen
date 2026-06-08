75703a9e911c67b79837a4cf0362084ccf3e0576
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2026-06-08 17:46:54 +0200

    initial

---
+++ Nitrogen/Nitrogen.cpp
@@ -0,0 +1,12 @@
+﻿// Nitrogen.cpp: definisce il punto di ingresso dell'applicazione.
+//
+
+#include "Nitrogen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
