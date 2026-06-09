c69b417ad5199bc0fb722171f7196ce6ac19556e
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2026-06-09 11:30:40 +0200

    yt-dlp downloader

--- Nitrogen/Nitrogen.cpp
+++ Nitrogen/Nitrogen.cpp
@@ -5,10 +5,9 @@
 
 using namespace std;
 
-static string general_args = "--no-write-thumbnail --no-cookies --no-playlist -N 5 --cache-dir \"./cache\" --no-write-subs --embed-subs --no-embed-thumbnail --no-embed-metadata --no-embed-chapters --no-embed-info-json --abort-on-error --no-batch-file --windows-filenames ";
+static string general_args = "--no-write-thumbnail --no-cookies --no-exec --no-playlist -N 5 --cache-dir \"./cache\" --no-write-subs --embed-subs --no-embed-thumbnail --no-sponsorblock --ffmpeg-location \"./ffmpeg/\" --no-embed-metadata --no-embed-chapters --no-embed-info-json --abort-on-error --no-batch-file --windows-filenames ";
 static string print_output_args = "--quiet --no-warnings --newline --print \"before_dl:STEP|Downloading %(title)s\" --print \"after_move:SUCCESS|%(filepath)s\" --print \"after_move:SUCCESS|%(filepath)s\" --progress --progress-template \"download:PROGRESS|%(progress._percent)s\" ";
-
-static string output_template = "-o \"{}.%(ext)s\" ";
+static string output_template = "-o \"{ID}.{EXT}\" -t {EXT}";
 
 #if _WIN32
 static string ffmpeg_tar_url = "https://huggingface.co/buckets/lu2000luk/portable/resolve/ffmpeg_win.tar?download=true";
@@ -16,6 +15,12 @@ static string ffmpeg_tar_url = "https://huggingface.co/buckets/lu2000luk/portabl
 static string ffmpeg_tar_url = "https://huggingface.co/buckets/lu2000luk/portable/resolve/ffmpeg_linux.tar?download=true";
 #endif
 
+
+#if _WIN32
+static string yt_dlp_url = "https://github.com/yt-dlp/yt-dlp/releases/download/2026.03.17/yt-dlp.exe";
+#else
+static string yt_dlp_url = "https://github.com/yt-dlp/yt-dlp/releases/download/2026.03.17/yt-dlp";
+#endif
 
 string escape_string(string input) {
 	string escaped;
@@ -82,12 +87,27 @@ void ffmpeg_setup() {
 	}
 }
 
-
+void yt_dlp_setup() {
+#if _WIN32
+	if (!exists("./yt-dlp.exe")) {	
+#else
+	if (!exists("./yt-dlp")) {
+#endif
+		cout << "Downloading yt-dlp..." << endl;
+#if _WIN32
+		string command = "curl.exe -L \"" + yt_dlp_url + "\" -o yt-dlp.exe";
+#else
+		string command = "curl -L \"" + yt_dlp_url + "\" -o yt-dlp";
+#endif
+		system(command.c_str());
+	}
+}
 
 int main()
 {
 	ffmpeg_setup();
+	yt_dlp_setup();
 
-	cout << "ARGS: " << general_args << print_output_args << output_template << endl;
+	cout << "USING ARGS: " << general_args << print_output_args << endl;
 	return 0;
 }
